9183d5697b068857e12c48bd60faee0c58b74196
Kiel Dowdle <dowdlek@msoe.edu>
2020-10-23 22:45:06 -0500

    mosaic and hotel

---
+++ HotelWiring.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class HotelWiring {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        long cases = Integer.parseInt(reader.readLine());
+
+        for (int c = 0; c < cases; c++) {
+            //nmk
+
+            String[] line = reader.readLine().split(" ");
+
+            int floors = Integer.parseInt(line[0]);
+            long rooms = Integer.parseInt(line[1]);
+            long switches = Integer.parseInt(line[2]);
+
+            long[] correct = new long[floors];
+
+
+            for (int f = 0; f < floors; f++) {
+                correct[f] = Integer.parseInt(reader.readLine());
+            }
+
+            Arrays.sort(correct);
+
+            long withPower = 0;
+
+            int currentIndex = 0;
+
+            for (int i = 0; i < switches; i++) {
+                withPower += rooms - correct[i];
+                currentIndex++;
+            }
+            for (int i = currentIndex; i < correct.length; i++) {
+                withPower += correct[i];
+            }
+
+
+            System.out.println(withPower);
+
+        }
+    }
+}
---
+++ mosaicDecoration.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class mosaicDecoration {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] line = reader.readLine().split(" ");
+
+        long n = Integer.parseInt(line[0]);
+        long b = Integer.parseInt(line[1]);
+        long p = Integer.parseInt(line[2]);
+        long bstacks = 0;
+        long pstacks = 0;
+
+        long totalb = 0;
+        long totalp = 0;
+
+        for (int c = 0; c < n; c++) {
+            line = reader.readLine().split(" ");
+
+            long tb = Integer.parseInt(line[0]);
+            long tp = Integer.parseInt(line[1]);
+
+            totalb += tb;
+            totalp += tp;
+        }
+
+        bstacks = totalb / 10;
+
+        if (bstacks * 10 < totalb) {
+            bstacks++;
+        }
+
+        pstacks = totalp / 10;
+
+        if (pstacks * 10 < totalp) {
+            pstacks++;
+        }
+
+        System.out.println(bstacks * b + pstacks * p);
+    }
+}
