4a82a36cdbb2fbd56368efa0cfb71bfafafe20ec
Nick Johnson <sylvyrfysh@gmail.com>
2020-10-27 15:51:16 -0500

    Kotlinize

--- ARMConstantMultiplication.kt
+++ ARMConstantMultiplication.kt
@@ -13,49 +13,67 @@ fun main() {
             bw.newLine()
         } else if (mulBy == 1L) {
             //nothing
-        } else if (java.lang.Long.bitCount(mulBy) == 1) {
-            bw.append("MOV R0, R0, LSL #").append(java.lang.Long.numberOfTrailingZeros(mulBy).toString())
+        } else if (mulBy.countOneBits() == 1) {
+            bw.append("MOV R0, R0, LSL #").append(mulBy.countTrailingZeroBits().toString())
             bw.newLine()
-        } else if (java.lang.Long.bitCount(mulBy) == 2) {
+        } else if (mulBy.countOneBits() == 2) {
             if (mulBy and 1L != 1L) {
-                bw.append("MOV R0, R0, LSL #").append(java.lang.Long.numberOfTrailingZeros(mulBy).toString())
+                bw.append("MOV R0, R0, LSL #").append(mulBy.countTrailingZeroBits().toString())
                 bw.newLine()
             }
             bw.append("ADD R0, R0, R0, LSL #").append(
-                (1 + java.lang.Long.numberOfTrailingZeros(
-                    (mulBy ushr (java.lang.Long.numberOfTrailingZeros(mulBy) + 1))
-                )).toString()
+                (1 + (mulBy ushr (mulBy.countTrailingZeroBits() + 1)).countTrailingZeroBits()).toString()
             )
             bw.newLine()
-        } else if (java.lang.Long.bitCount(mulBy) == 32) {
+        } else if (mulBy.countOneBits() == 32) {
             bw.append("RSB R0, R0, R0, LSL #16")
             bw.newLine()
             bw.append("ADD R0, R0, R0, LSL #16")
             bw.newLine()
-        } else if (java.lang.Long.bitCount(mulBy) == 1 + java.lang.Long.numberOfTrailingZeros(
-                java.lang.Long.highestOneBit(
-                    mulBy
-                ) / java.lang.Long.lowestOneBit(mulBy)
-            )
-        ) {
-            bw.append("RSB R0, R0, R0, LSL #").append(java.lang.Long.bitCount(mulBy).toString())
+        } else if (mulBy.countOneBits() == 1 + (mulBy.takeHighestOneBit() / mulBy.takeLowestOneBit()).countTrailingZeroBits()) {
+            bw.append("RSB R0, R0, R0, LSL #").append(mulBy.countOneBits().toString())
             bw.newLine()
             if (mulBy and 1L != 1L) {
-                bw.append("MOV R0, R0, LSL #").append(java.lang.Long.numberOfTrailingZeros(mulBy).toString())
+                bw.append("MOV R0, R0, LSL #").append(mulBy.countTrailingZeroBits().toString())
                 bw.newLine()
             }
         } else {
             val extRsbs = ArrayList<Long>()
-            for (i in longArrayOf(1048575, 514287, 262143, 131071, 65535, 32767, 16383, 8191, 4095, 2047, 1023, 511, 255, 127, 63, 31, 15, 7, 3)) {
+            for (i in longArrayOf(65535, 32767, 16383, 8191, 4095, 2047, 1023, 511, 255, 127, 63, 31, 15, 7, 3)) {
                 while (mulBy % i == 0L) {
                     mulBy /= i
                     extRsbs += i
                 }
             }
             if (mulBy != 1L) {
+                var lChar = '\u0000'
+                var cLen = 0
+                var cpos = 0
+                val charLens = ArrayList<Triple<Int, Char, Int>>()
+                val binStr = mulBy.toString(2)
+                for (c in binStr) {
+                    if (c == lChar) {
+                        ++cLen
+                    } else {
+                        if (cLen != 0) {
+                            charLens += Triple(cLen, lChar, cpos)
+                            cpos += cLen
+                        }
+                        lChar = c
+                        cLen = 1
+                    }
+                }
+                if (cLen != 0) {
+                    charLens += Triple(cLen, lChar, cpos)
+                }
+                val maxOne = charLens.filter { it.second == '1' }.maxByOrNull { it.first }!!
+                if (maxOne.first >= 3) {
+                    val xr = (1L shl maxOne.first) - 1L
+                    mulBy = mulBy xor (xr shl (mulBy.toString(2).length - maxOne.first - maxOne.third))
+                }
                 var shr = mulBy / 2
                 var shAmt = 1
-                while (shr != 1L) {
+                while (shr > 1L) {
                     if (shr and 1L == 1L) {
                         bw.append("ADD R1, ${if (mulBy and 1L == 1L && shAmt == 1) "R0" else "R1"}, R0, LSL #").append(shAmt.toString())
                         bw.newLine()
@@ -66,11 +84,19 @@ fun main() {
                     ++shAmt
                     shr /= 2
                 }
-                    bw.append("ADD R0, R1, R0, LSL #").append(shAmt.toString())
+                if (maxOne.first >= 3) {
+                    bw.append("RSB R2, R0, R0, LSL #").append(maxOne.first.toString())
                     bw.newLine()
+                    bw.append("ADD R0, R0, R2, LSL #").append((binStr.length - maxOne.first - maxOne.third).toString())
+                    bw.newLine()
+                }
+                if (shr >= 1L) {
+                    bw.append("ADD R0, R1, R0, LSL #").append((shAmt - if (mulBy <= 2L) 1 else 0).toString())
+                    bw.newLine()
+                }
             }
             extRsbs.forEach { num ->
-                bw.append("RSB R0, R0, R0, LSL #").append(java.lang.Long.bitCount(num).toString())
+                bw.append("RSB R0, R0, R0, LSL #").append(num.countOneBits().toString())
                 bw.newLine()
             }
         }
